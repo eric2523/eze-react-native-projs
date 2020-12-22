0b414bbcdb1edb660f4883a9b10eafc036f669e4
eric2523@gmail.com <eric2523@gmail.com>
2020-12-21 16:22:18 -0800

    add header

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Header } from "./components/header.js"
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <Header></Header>
     </View>
   )
 }
@@ -12,8 +13,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    paddingTop: 60
   },
   text: {
     color: "darkslateblue",
