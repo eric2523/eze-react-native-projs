d4493d44990e41314df33e757e271fbe705ef497
eric2523@gmail.com <eric2523@gmail.com>
2020-12-21 16:38:02 -0800

    add styling and X

--- ShoppingList/components/list-item.js
+++ ShoppingList/components/list-item.js
@@ -1,10 +1,30 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export const ListItem = ({item}) => {
   return (
-    <View>
-      <Text>{item.text}</Text>
-    </View>
-  )
-}
+    <TouchableOpacity style={styles.listItem}>
+      <View style={styles.listItemView}>
+        <Text style={styles.listItemText}>{item.text}</Text>
+        <Text>X</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 15,
+    backgroundColor: "#f8f8f8",
+    borderBottomWidth: 1,
+    borderColor: "#eee"
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+  listItemView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  }
+})
